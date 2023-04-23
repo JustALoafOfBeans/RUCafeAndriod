90624073d83a7b764b2366b6ed578a9a90938bb3
JustALoafOfBeans <victoriachen4520@gmail.com>
2023-04-23 02:28:34 -0400

    can open coffee page, formatting on main and coffee

--- app/src/main/java/com/example/rucafeandriod/MainActivity.java
+++ app/src/main/java/com/example/rucafeandriod/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.rucafeandriod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageButton coffeeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        coffeeButton = (ImageButton) findViewById(R.id.coffeeButton);
+        coffeeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCoffeeActivity();
+            }
+        });
+    }
+
+    public void openCoffeeActivity() {
+        Intent coffeeIntent = new Intent(this, CoffeeActivity.class);
+        startActivity(coffeeIntent);
     }
 }
