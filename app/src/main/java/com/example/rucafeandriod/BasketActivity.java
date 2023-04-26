154ba630f4a6c26db4e89d80432dd2b3962f1d17
JustALoafOfBeans <victoriachen4520@gmail.com>
2023-04-26 13:30:04 -0400

    view basket and prices update

--- app/src/main/java/com/example/rucafeandriod/BasketActivity.java
+++ app/src/main/java/com/example/rucafeandriod/BasketActivity.java
@@ -3,12 +3,56 @@ package com.example.rucafeandriod;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BasketActivity extends AppCompatActivity {
+    private ListView basketContents;
+    private ArrayList<MenuItem> currentBasket;
+    private TextView subtotal, tax, total;
+    /**
+     *  Constant for tax rate in NJ, applied to find tax and total costs
+     */
+    private static final double TAXNJ = 0.06625;
+    /**
+     * Use DF.format(value) to round value to two decimals places. Rounding
+     * up or down is by basic convention. Returns a String.
+     */
+    private static final DecimalFormat DF = new DecimalFormat("0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basket);
+        basketContents = (ListView) findViewById(R.id.basket_list);
+        subtotal = (TextView) findViewById(R.id.subtotal);
+        tax = (TextView) findViewById(R.id.tax);
+        total = (TextView) findViewById(R.id.total);
+        currentBasket = MainActivity.itemsInBasket;
+        displayBasket();
+    }
+
+    private void displayBasket() {
+        ArrayAdapter<MenuItem> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, currentBasket);
+        basketContents.setAdapter(arrayAdapter);
+        updateTotals();
+    }
+
+    private void updateTotals() {
+        double sum = 0;
+        for (MenuItem item : currentBasket){
+            sum += item.itemPrice();
+        }
+        subtotal.setText("Subtotal: $" + sum);
+        double taxValue = Double.valueOf(DF.format(sum*TAXNJ));
+        tax.setText("Tax: $" + taxValue);
+        double totalValue = Double.valueOf(DF.format(sum + sum*TAXNJ));
+        total.setText("Total: $" + totalValue);
     }
 }
--- app/src/main/java/com/example/rucafeandriod/MainActivity.java
+++ app/src/main/java/com/example/rucafeandriod/MainActivity.java
@@ -22,23 +22,24 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton donutButton, coffeeButton, basketButton, storeOrdersButton;
-    private ArrayList<MenuItem> orderDonut;
-    private ArrayList<MenuItem> orderCoffee;
+    /**
+     * Single array list to hold all items in basket. Makes it easier so only have to pass one item between activities.
+     */
+    public static ArrayList<MenuItem> itemsInBasket;
     private ActivityResultLauncher<Intent> activityResultLaunch = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
         @Override
         public void onActivityResult(ActivityResult result) {
             // watched https://www.youtube.com/watch?v=Ke9PaRdMcgc
             if (result != null && result.getResultCode() == RESULT_OK) {
-                if(result.getData() != null && result.getData().getSerializableExtra("c") != null) {
+                if(result.getData() != null && result.getData().getSerializableExtra("c") != null) { //retrieve coffee to add
                     Coffee cup = (Coffee) result.getData().getSerializableExtra("c");
-                    System.out.println(cup);
+                    addCoffee(cup);
+                } else if (result.getData() != null && result.getData().getSerializableExtra("b") != null) { //retrieve updated basket
+                    System.out.println("Success");
                 }
             }
         }
     });
-
-    private static int DONUTCODE = 123;
-    private static int COFFEECODE = 456;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,22 +48,11 @@ public class MainActivity extends AppCompatActivity {
         instantiateButtons();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    private void addDonut(ActivityResult result) {
-        // need to implement, ignore nonesense below
-
-        //Intent data = result.getData();
-        //System.out.println(data.toString());
-        //Coffee data = (Coffee) getIntent().getSerializableExtra("coffee");
-        //System.out.println(data);
-        /**if(getActivity().getIntent().getExtras().getSerializable("coffee") != null) {
-            Coffee data = (Coffee) getIntent().getExtras().getSerializable("coffee");
-            System.out.println(data.toString());
-        } */
+    private void addCoffee(Coffee toAdd) {
+        if (itemsInBasket == null) {
+            itemsInBasket = new ArrayList<>();
+        }
+        itemsInBasket.add(toAdd);
     }
 
     private void instantiateButtons() {
@@ -110,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void openBasketActivity() {
         Intent basketIntent = new Intent(this, BasketActivity.class);
-        startActivity(basketIntent);
+        activityResultLaunch.launch(basketIntent);
     }
 
     public void openDonutActivity() {
